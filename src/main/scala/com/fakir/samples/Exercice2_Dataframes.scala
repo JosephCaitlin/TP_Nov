0b292606ce1f8f347eb198df05837e60d7abd6dc
Joseph Caitlin <josephcaitlin@macbook-pro-de-caitlin.home>
2020-11-23 07:56:04 -0400

    final

--- src/main/scala/com/fakir/samples/Exercice2_Dataframes.scala
+++ src/main/scala/com/fakir/samples/Exercice2_Dataframes.scala
@@ -14,10 +14,10 @@ object Exercice2_Dataframes {
     //
     val df: DataFrame = sparkSession.read.option("delimiter", ";").option("inferSchema", true).option("header", false).csv("data/donnees.csv")
     //Question 2 : Nommez les colonnes comme suit : nom_film, nombre_vues, note_film, acteur_principal
-    val df1= df.withColumnRenamed("_c0", "nom_film")
-    val df2= df.withColumnRenamed("_c1", "nombre_vues")
-    val df3= df.withColumnRenamed("_c2", "note_film")
-    val df4= df.withColumnRenamed("_c3", "acteur_principal")
+    val df= df.withColumnRenamed("_c0", "nom_film")
+    val df= df.withColumnRenamed("_c1", "nombre_vues")
+    val df= df.withColumnRenamed("_c2", "note_film")
+    val df= df.withColumnRenamed("_c3", "acteur_principal")
     df.show
 
 
@@ -31,9 +31,10 @@ object Exercice2_Dataframes {
     //moyenne_leonardo.show
 
     //4.Quel est le pourcentage de vues des films de Di Caprio par rapport à l'échantillon que nous avons ?
-    val vues_leonardo = df.groupBy("_c3").sum("_c1").filter("_c3") == "Di Caprio")
-    val vues_totales = df.groupBy().sum("_c1")
+    //val vues_leonardo = df.groupBy("_c3").sum("_c1").filter("_c3") == "Di Caprio")
+    //val vues_totales = df.groupBy().sum("_c1")
     //val pourcentage = (vues_leonardo / vues_totales) * 100
+    //println(pourcentage)
 
 // Question 4 :Créer une nouvelle colonne dans ce DataFrame, "pourcentage de vues", contenant le pourcentage de vues pour chaque film (combien de fois le film a-t-il été vu par rapport aux vues globales ?)
 }
